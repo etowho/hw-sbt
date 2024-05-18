58a12c19fbcd0c00cc94acd9942e38d799425be2
etowho <94126387+etowho@users.noreply.github.com>
2024-05-18 18:19:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,48 +1,37 @@
 from flask import Flask, jsonify
 from datetime import datetime
+import requests
 
 app = Flask(__name__)
 
-# Счетчик для подсчета количества обращений к /time endpoint'у
+# Счетчик для подсчета количества обращений к /time endpoint
 time_requests_count = 0
-
-# Список для хранения информации о каждом обращении к /time endpoint'у
-time_requests_info = []
-
-
-def record_time_request():
-    """Записывает информацию о текущем обращении к /time endpoint'у."""
-    global time_requests_count
-    global time_requests_info
-
-    # Увеличиваем счетчик обращений к /time endpoint'у
-    time_requests_count += 1
-
-    # Записываем информацию о текущем обращении
-    time_requests_info.append({
-        'timestamp': datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    })
-
 
 # Endpoint для получения текущего времени
 @app.route('/time', methods=['GET'])
 def get_current_time():
-    """Возвращает текущее время."""
-    record_time_request()
-
-    # Возвращаем текущее время
-    return jsonify({'current_time': datetime.now().strftime('%Y-%m-%d %H:%M:%S')})
-
-
-# Endpoint для получения статистики обращений к /time endpoint'у
-@app.route('/statistics', methods=['GET'])
-def get_time_requests_statistics():
-    """Возвращает количество обращений к /time endpoint'у."""
     global time_requests_count
 
-    # Возвращаем количество обращений к /time endpoint'у
-    return jsonify({'time_requests_count': time_requests_count})
+    # Увеличиваем счетчик обращений к /time endpoint
+    time_requests_count += 1
 
+    # Отправка запроса к внешнему API для получения текущего времени
+    response = requests.get('http://worldtimeapi.org/api/timezone/Europe/Moscow')
+    if response.status_code == 200:
+        current_time = response.json().get('datetime', 'Нет данных о времени')
+    else:
+        current_time = f"Ошибка при запросе: {response.status_code}"
+
+    # Возвращаем текущее время
+    return jsonify({'current_time': current_time})
+
+# Endpoint для получения статистики обращений к /time endpoint
+@app.route('/statistics', methods=['GET'])
+def get_time_requests_statistics():
+    global time_requests_count
+
+    # Возвращаем количество обращений к /time endpoint
+    return jsonify({'time_requests_count': time_requests_count})
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True, host='0.0.0.0')
